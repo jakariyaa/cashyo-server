8169845369b3401b6872b3cd94783c76e74ff506
jakariyaa <jakariya3460@gmail.com>
2025-10-07 16:14:54 +0600

    Modify Vercel Config

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,24 @@
-import dotenv from "dotenv";
+import "dotenv/config";
+import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/digital_wallet";
+let server: Server;
 
 // Server bootstrap
-function bootstrap() {
-  mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log("Connected to MongoDB");
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB:", error.message);
-      console.log("Starting server without database connection...");
-      app.listen(PORT, () => {
-        console.log(
-          `Server running on port ${PORT} (without database connection)`
-        );
-      });
+async function bootstrap() {
+  try {
+    const { connection } = await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB at", connection.host);
+    server = app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
     });
+  } catch (error) {
+    console.error("Error starting the server \n", error);
+  }
 }
+
+bootstrap();
